6e302927558f33bab397568e975fdc365e14e4ef
swarali-santosh-desai <76898834+swarali-santosh-desai@users.noreply.github.com>
2024-10-29 15:05:22 +0530

    Update E-STOP_TRANSMITTER.ino

--- E-STOP_TRANSMITTER.ino
+++ E-STOP_TRANSMITTER.ino
@@ -1,34 +1,26 @@
 #include <WiFi.h>
 #include <esp_now.h>
 
-
 uint8_t receiverAddress[] = {0x24, 0x0A, 0xC4, 0xD8, 0xE4, 0xB8}; 
-
-
-typedef struct struct_message {
-  bool estop;
-} struct_message;
-
-struct_message estopData;
-
-void onSent(const uint8_t *macAddr, esp_now_send_status_t status) {
-  Serial.print("Delivery Status: ");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Success" : "Fail");
-}
+bool estop = false; 
+int buttonPin = 4;  
+int relayPin = 5;   
 
 void setup() {
   Serial.begin(115200);
 
-  // Initialize WiFi
-  WiFi.mode(WIFI_STA);
+  pinMode(buttonPin, INPUT);
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, HIGH); 
 
-  
+ 
+  WiFi.mode(WIFI_STA);
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-  
-  
+
+
   esp_now_peer_info_t peerInfo;
   memcpy(peerInfo.peer_addr, receiverAddress, 6);
   peerInfo.channel = 0;  
@@ -38,21 +30,19 @@ void setup() {
     Serial.println("Failed to add peer");
     return;
   }
-
-  esp_now_register_send_cb(onSent);
 }
 
 void loop() {
-  estopData.estop = digitalRead(0);
+  
+  if (digitalRead(buttonPin) == HIGH) {
+    estop = true;  
+    digitalWrite(relayPin, LOW); 
 
  
-  esp_err_t result = esp_now_send(receiverAddress, (uint8_t *) &estopData, sizeof(estopData));
+    uint8_t stopCommand[] = {0x01};
+    esp_now_send(receiverAddress, stopCommand, sizeof(stopCommand));
+    Serial.println("Emergency Stop Command Sent!");
 
-  if (result == ESP_OK) {
-    Serial.println("Sent successfully");
-  } else {
-    Serial.println("Error sending data");
+    delay(500); 
   }
-
-  delay(100); 
 }
